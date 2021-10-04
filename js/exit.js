51c5abf2b90006ff27bed880d2a20e46c5617c90
mikechenczy <88608181+mikechenczy@users.noreply.github.com>
2021-10-04 20:42:49 +0800

    Update exit.js

--- js/exit.js
+++ js/exit.js
@@ -1,7 +1,7 @@
 var backendAddress = "server.mjczy.xyz:"+(('https:' === document.location.protocol)?"8764":"8765")
 $(function() {
     $.ajax({
-        url: '//' + backendAddress + '/api/exit'+"&domain="+document.domain,
+        url: '//' + backendAddress + '/api/exit'+"?domain="+document.domain,
         dataType: "jsonp",//数据类型为jsonp
         jsonp: "callback",//服务端返回回调方法名
         success: function (data) {
