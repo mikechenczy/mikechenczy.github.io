9a93d77c47291d6a4f966aff1678f7bf867cdba2
mikechenczy <88608181+mikechenczy@users.noreply.github.com>
2021-10-04 20:36:58 +0800

    changed

--- js/checklogin.js
+++ js/checklogin.js
@@ -3,9 +3,10 @@ $(function() {
     $("#avatar").hide();
     var username = getCookie('username')
     var password = getCookie("password")
+    if(username != null && username !== "" && password != null && password !== "" && check(username, password)) {
         $.ajax({
             url: '//' + backendAddress + '/api/login?username=' + encodeURIComponent(username) + "&password=" + password
-            +"&domain="+document.domain,
+                + "&domain=" + document.domain,
             dataType: "jsonp",//数据类型为jsonp
             jsonp: "callback",//服务端返回回调方法名
             success: function (data) {
@@ -17,4 +18,5 @@ $(function() {
                 }
             }
         });
+    }
 });
--- js/checkloginback.js
+++ js/checkloginback.js
@@ -3,10 +3,10 @@ $(function() {
     $("#avatar").hide();
     var username = getCookie('username')
     var password = getCookie("password")
-    if(username != null && username !== "" && password != null && password !== "" && check(username, password)){
-
+    if(username != null && username !== "" && password != null && password !== "" && check(username, password)) {
         $.ajax({
-            url: '//' + backendAddress + '/api/login?username=' + encodeURIComponent(username) + "&password=" + password,
+            url: '//' + backendAddress + '/api/login?username=' + encodeURIComponent(username) + "&password=" + password
+                + "&domain=" + document.domain,
             dataType: "jsonp",//数据类型为jsonp
             jsonp: "callback",//服务端返回回调方法名
             success: function (data) {
