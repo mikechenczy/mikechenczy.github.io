2965e0ef9ea7d89591a6e2ad0666028f87354861
mikechenczy <88608181+mikechenczy@users.noreply.github.com>
2024-02-01 14:10:59 +0800

    changed

--- account/script.js
+++ account/script.js
@@ -4,12 +4,21 @@ if(getCookie("username")==null || getCookie("password")==null) {
 }
 $.ajax({
     url: debug?"https://mjczy.top/getIp":"getIp",
+    dataType: "text",
     success: function (data) {
+        console.log(data);
         var pattern = /((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)/;
-        console.log(pattern.exec(data)[0]);
+        var ip = "";
+        if(pattern.exec(data)==null) {
+            pattern = /(([\da-fA-F]{1,4}):){7}([\da-fA-F]{1,4})/;
+            if(pattern.exec(data)!=null) {
+                ip = pattern.exec(data)[0];
+            }
+        }
+        console.log(ip);
         $.ajax({
             url: urlPrefix+'user/login?device=web&username=' + encodeURIComponent(getCookie('username')) + '&password='
-                + encodeURIComponent(getCookie('password')) + '&ipAddress=' + encodeURIComponent(pattern.exec(data)[0]),
+                + encodeURIComponent(getCookie('password')) + '&ipAddress=' + encodeURIComponent(ip),
             dataType : "json",
             success: function (data) {
                 console.log(data);
--- js/check.js
+++ js/check.js
@@ -18,12 +18,21 @@ var password = getCookie("password")
 if(username != null && username !== "" && password != null && password !== "" && check(username, password)) {
     $.ajax({
         url: debug?"https://mjczy.top/getIp":"getIp",
+        dataType: "text",
         success: function (data) {
+            console.log(data);
             var pattern = /((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)/;
-            console.log(pattern.exec(data)[0]);
+            var ip = "";
+            if(pattern.exec(data)==null) {
+                pattern = /(([\da-fA-F]{1,4}):){7}([\da-fA-F]{1,4})/;
+                if(pattern.exec(data)!=null) {
+                    ip = pattern.exec(data)[0];
+                }
+            }
+            console.log(ip);
             $.ajax({
                 url: urlPrefix+'user/login?device=web&username=' + encodeURIComponent(username) + '&password='
-                    + encodeURIComponent(password) + '&ipAddress=' + encodeURIComponent(pattern.exec(data)[0]),
+                            + encodeURIComponent(password) + '&ipAddress=' + encodeURIComponent(ip),
                 dataType : "json",
                 success: function (data) {
                     switch (data.errno) {
